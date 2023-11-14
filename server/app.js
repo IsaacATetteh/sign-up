b4e072a088894eada91489700f9a524c2bd053b2
IsaacATetteh <iabbeytetteh@gmail.com>
2023-11-14 20:55:00 +0000

    MongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const mongoUrl = "mongodb+srv://Isaac:123@cluster0.prn0cfs.mongodb.net/";
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((e) => console.log(e));
+
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Server stared");
@@ -7,4 +16,14 @@ app.listen(3000, () => {
 
 app.post("/post", async (req, res) => {
   console.log(req.body);
+  const { data } = req.body;
+  try {
+    if (data === "bob") {
+      res.send({ status: "bob" });
+    } else {
+      res.send({ status: "user not found" });
+    }
+  } catch (error) {
+    res.send({ status: "error" });
+  }
 });
