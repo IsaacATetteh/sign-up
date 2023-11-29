c756f7f91a62c355434dafbbae0b114e274cde43
ia313 <ia313@kent.ac.uk>
2023-11-29 15:28:32 +0000

    Updated Frontend

--- server/userDetails.js
+++ server/userDetails.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserDetailsSchema = new mongoose.Schema(
   {
-    email: { type: String, unique: true },
+    name: { type: String, unique: true },
     password: String,
     email: { type: String, unique: true },
   },
